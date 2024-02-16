2e817ae617548044cff75e6781091260f4f046bf
mahmoud hassan <mahmoudhs236@gmail.com>
2024-02-16 17:38:53 +0200

    create home view & AppBar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -6,7 +6,18 @@ class NewsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title:const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+             Text("News", style: TextStyle(color: Colors.black),),
+             Text(" Cloud", style: TextStyle(color: Colors.orange),),
+          ],
+        ),
+      ),
       body: const Column(
         children: [],
       ),
