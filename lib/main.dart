90114d9324079f36b315e1b0cb8a6bd37d325db8
mahmoud hassan <mahmoudhs236@gmail.com>
2024-02-16 17:23:43 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'views/home_view.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:const NewsView(),
+    );
+  }
+}
+
---
+++ lib/views/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsView extends StatelessWidget {
+  const NewsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
